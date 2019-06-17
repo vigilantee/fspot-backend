3b140afd95454453ca3d7e5bafb60cd33ef33958
vigilantee <abhishekjhaswe@gmail.com>
2019-06-17 07:14:46 +0530

    added filters

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 const express=require('express');
+const url=require('url');
 const db=require('../db/index.js');
 const router=express.Router();
 
 router.get('/user_info',(req,res,next)=>{
     try{
-        db.executeQuery('select id, username from user', results=>res.json(results));
+        db.executeQuery('select id, username, profile_pic from user', results=>res.json(results));
     }catch(e){
         console.log('Manish Chaava',e);
         res.sendStatus(500);
@@ -14,9 +15,23 @@ router.get('/user_info',(req,res,next)=>{
 router.get('/user_info/:uid',(req,res,next)=>{
     try{
 
-        db.executeQuery(`select id, username from user where id=${req.params.uid}`, results=>res.json(results[0]));
+        db.executeQuery(`select id, username, profile_pic from user where id=${req.params.uid}`, results=>res.json(results[0]));
     }catch(e){
-        console.log('Manish Chaava',e);
+        console.log('Error logged....',e);
+        res.sendStatus(500);
+    }
+});
+
+router.get('/menu',(req,res,next)=>{
+    try{
+        let filters = '1 ';
+        if(req.query.veg)
+            filters += 'and type = "veg" ';
+        else if(req.query.nonveg)
+            filters += 'and type = "non-veg" ';
+        db.executeQuery(`select * from product where ${filters}`, results=>res.json(results));
+    }catch(e){
+        console.log('Error logged....',e);
         res.sendStatus(500);
     }
 });
